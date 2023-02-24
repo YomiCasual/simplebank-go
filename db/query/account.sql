6cb527463641c167b13448ea7607a707ec248f94
AbiodunOnisade <abiodun.onisade@alerzo.com>
2023-02-24 17:57:53 +0100

    feat: bootstrap and added sqlc

---
+++ db/query/account.sql
@@ -0,0 +1,26 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner, balance, currency
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY owner;
+
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+  set balance = $2
+WHERE id = $1
+RETURNING *;
